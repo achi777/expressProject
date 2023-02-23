cfe90d15c2ce6a134c43fef3f88edfb4484fee89
achi <archil.odishelidze@gmail.com>
2023-02-23 23:25:34 +0400

    e2e

--- src/app.ts
+++ src/app.ts
@@ -50,4 +50,8 @@ export class App {
 		this.server = this.app.listen(this.port);
 		this.logger.log(`server run http://localhost:${this.port}`);
 	}
+
+	public close(): void {
+		this.server.close();
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -32,12 +32,12 @@ const appBuindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<App>(TYPES.Application).to(App);
 });
 
-function bootstrap(): IBootstrapReturn {
+async function bootstrap(): Promise<IBootstrapReturn> {
 	const appContainer = new Container();
 	appContainer.load(appBuindings);
 	const app = appContainer.get<App>(TYPES.Application);
-	app.init();
+	await app.init();
 	return { appContainer, app };
 }
 
-export const { app, appContainer } = bootstrap();
+export const boot = bootstrap();
